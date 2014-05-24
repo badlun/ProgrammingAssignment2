2f5e40896a93fb0e79c18b74b3a4215d6842b894
Ikuko Mukai <ikuko.mukaicheh@gmail.com>
2014-05-24 10:01:02 -0500

    Working progress of the assignment 2
    Examples for assignment 2 and the 1st draft of cache matrix.R.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cim <- NULL # Initialize the cached inversed matrix of x
+    set <- function(y) { # Set a new matrix
+        x <<- y
+        cim <<- NULL # Initialize the cached inversed matrix for the new matrix
+    }
+    get <- function() x
+    setinverse <- function(inverse) cim <<- inverse
+    getinverse <- function() cim
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+    
 }
 
 
