04ef17b40eb5961702081f4e6ab0e0f01b3c99f1
Ikuko Mukai <ikuko.mukaicheh@gmail.com>
2014-05-25 11:28:22 -0500

    Final version of assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions compute the inverse of a newly set matrix x once and cache
+## the inverse matrix of x.
 
-## Write a short comment describing this function
-
+## This function saves a matrix x and the inversed matrix of x in cache.
+## It returns a list of 4 functions that can be used to access the the 
+## matrix x and the inversed matrix of x.
 makeCacheMatrix <- function(x = matrix()) {
-    cim <- NULL # Initialize the cached inversed matrix of x
+    cim <- NULL # Initialize cached inversed matrix for a matrix x
     set <- function(y) { # Set a new matrix
         x <<- y
-        cim <<- NULL # Initialize the cached inversed matrix for the new matrix
+        cim <<- NULL # Initialize the cached inversed matrix for a new matrix
     }
-    get <- function() x
-    setinverse <- function(inverse) cim <<- inverse
-    getinverse <- function() cim
+    get <- function() x # Returns matrix x 
+    setinverse <- function(inverse) cim <<- inverse # Saves inversed matrix of x
+    getinverse <- function() cim # Returns cached inversed matrix of x 
+    
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
@@ -19,8 +21,18 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## This function returns the inverse matrix  of 'x' from the cache.   
+## The inverse of x will be computed when it is not found in the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # Accesses cached data
+    cim <- x$getinverse()
+    if(!is.null(cim)) {
+        message("getting cached data")
+        return(cim)
+    }
+    # Computes the inversed matrix of x and cache it
+    data <- x$get()
+    cim <- solve(data, ...)
+    x$setinverse(cim)
+    cim    
 }
